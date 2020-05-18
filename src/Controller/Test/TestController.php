d8bf794e1962737b5bcd7a778b42a45c152ae960
lew <buzek.jonatan@gmail.com>
2020-05-18 08:41:29 +0200

    feat: google analytics

--- src/Controller/Test/TestController.php
+++ src/Controller/Test/TestController.php
@@ -13,7 +13,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
-
     /**
      * @var SessionInterface
      */
---
+++ tests/Controller/Test/TestControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Tests\Controller;
+
+
+use App\Controller\Test\TestController;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class TestControllerTest extends TestCase
+{
+
+}
